9a473e3aeed2b8d94d8e0eed0f73096ab664dc1d
joehsieh <joehsieh@kkbox.com>
2014-05-10 14:27:59 +0800

    Locks orientation and tunes a word.

--- TownCandidateMap/TownCandidateMap/TCDetailViewController.m
+++ TownCandidateMap/TownCandidateMap/TCDetailViewController.m
@@ -82,7 +82,7 @@
 
 - (void)splitViewController:(UISplitViewController *)splitController willHideViewController:(UIViewController *)viewController withBarButtonItem:(UIBarButtonItem *)barButtonItem forPopoverController:(UIPopoverController *)popoverController
 {
-    barButtonItem.title = NSLocalizedString(@"Master", @"Master");
+    barButtonItem.title = NSLocalizedString(@"Candidates", @"Candidates");
     [self.navigationItem setLeftBarButtonItem:barButtonItem animated:YES];
     self.masterPopoverController = popoverController;
 }
